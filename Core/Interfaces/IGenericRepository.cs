75f8ea693a21c2dcd0128b98739f6109d9067e39
Ryno Conradie <rynoconradie12@gmail.com>
2022-06-25 11:36:02 +0200

    Implemented Paging and item filters with sorting

--- Core/Interfaces/IGenericRepository.cs
+++ Core/Interfaces/IGenericRepository.cs
@@ -14,5 +14,7 @@ namespace Core.Interfaces
 
         Task<T> GetEntiryWithSpec(ISpecification<T> spec);
         Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
+
+        Task<int> CountAsync(ISpecification<T> spec);
     }
 }
--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -19,6 +19,21 @@ namespace Infrastructure.Data
                 quary = quary.Where(spec.Criteria);
             }
 
+            if(spec.OrderBy != null)
+            {
+                quary = quary.OrderBy(spec.OrderBy);
+            }
+
+            if(spec.OrderByDescending != null)
+            {
+                quary = quary.OrderByDescending(spec.OrderByDescending);
+            }
+
+            if(spec.IsPagingEnabled)
+            {
+                quary = quary.Skip(spec.Skip).Take(spec.Take);
+            }
+
             quary = spec.Includes.Aggregate(quary, (current, include) => current.Include(include));
 
             return quary;
