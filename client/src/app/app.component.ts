bf00a9fc9eaa93c173c976234a1ef643076b9035
Ryno Conradie <rynoconradie12@gmail.com>
2022-06-29 13:55:18 +0200

    Added Basket

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
+import { BasketService } from './basket/basket.service';
+import { IPagination } from './shared/models/pagination';
+import { IProduct } from './shared/models/product';
 
 @Component({
   selector: 'app-root',
@@ -7,12 +10,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  
-  title = 'Skinet';
+  title = 'SkiNet';
 
-  constructor() {}
+  constructor(private basketService: BasketService) { }
 
   ngOnInit(): void {
-    
+    const basketId = localStorage.getItem('basket_id');
+    if (basketId) {
+      this.basketService.getBasket(basketId).subscribe(() => {
+        console.log('initialised basket');
+      }, error => {
+        console.log(error);
+      })
+    }
   }
 }
